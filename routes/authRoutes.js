1a9dbdd19e4f154483f4573c3087d9013ebd863a
Abhay Gadhvi <gadhviabhay11@gmail.com>
2025-05-19 14:26:23 +0530

    Add files via upload

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,15 @@
+// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Authentication routes
+// These will be accessible at /api/auth/signup and /api/auth/login because of the prefix in server.js
+
+// User registration endpoint
 router.post('/signup', authController.signup);
+
+// User login endpoint
 router.post('/login', authController.login);
+
+module.exports = router;
