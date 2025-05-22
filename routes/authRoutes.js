ecce0cfcff535e157ffcc2c9bc1adcc24a98d24f
Abhay Gadhvi <gadhviabhay11@gmail.com>
2025-05-22 12:52:17 +0530

    Add files via upload

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,26 @@
-// // routes/authRoutes.js
+// // // routes/authRoutes.js
+
+// const express = require('express');
+// const router = express.Router();
+// const authController = require('../controllers/authController');
+
+// // User registration endpoint
+// router.post('/auth/signup', authController.signup);
+
+// // User login endpoint
+// router.post('/auth/login', authController.login);
+
+// module.exports = router;
+
 
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
 // User registration endpoint
-router.post('/auth/signup', authController.signup);
+router.post('/signup', authController.signup);
 
 // User login endpoint
-router.post('/auth/login', authController.login);
+router.post('/login', authController.login);
 
 module.exports = router;
