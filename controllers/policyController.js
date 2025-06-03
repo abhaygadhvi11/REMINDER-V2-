349af6eee935ab98da8630e3b14032b4e07fc907
Abhay Gadhvi <gadhviabhay11@gmail.com>
2025-06-03 12:11:01 +0530

    Add files via upload

--- controllers/policyController.js
+++ controllers/policyController.js
@@ -1,14 +1,35 @@
 const policyService = require('../services/policyService');
 
-// API to get all policies
+// // API to get all policies
+// const getAllPolicies = (req, res) => {
+//   policyService.getAllPolicies((error, policies) => {
+//     if (error) {
+//       return res.status(500).json({ message: 'Error fetching policies' });
+//     }
+//     //res.status(200).json({ message: 'Policies retrieved successfully', policies });
+//   });
+// };
+
 const getAllPolicies = (req, res) => {
   policyService.getAllPolicies((error, policies) => {
     if (error) {
-      return res.status(500).json({ message: 'Error fetching policies' });
+      return res.status(500).json([{ message: 'Error fetching policies' }]);
     }
-    res.status(200).json({ message: 'Policies retrieved successfully', policies });
+
+    res.status(200).json(policies); // direct array of policy objects
   });
 };
+
+// const getAllPolicies = (req, res) => {
+//   policyService.getAllPolicies((error, policies) => {
+//     if (error) {
+//       return res.status(500).json([{ message: 'Error fetching policies' }]);
+//     }
+
+//     // Assuming `policies` is already an array of objects like: [{ id: 1, name: 'Policy A' }, ...]
+//     res.status(200).json(policies);
+//   });
+// };
    
 // API to get policy by ID
 const getPolicyById = (req, res) => {
