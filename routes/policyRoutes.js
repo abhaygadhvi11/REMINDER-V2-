c51515a5d27d3a67467ca1cad9d6a7838522a9fd
Abhay Gadhvi <gadhviabhay11@gmail.com>
2025-06-03 12:12:08 +0530

    Add files via upload

--- routes/policyRoutes.js
+++ routes/policyRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const authenticateToken = require('../middlewares/authMiddleware');
 const policyController = require('../controllers/policyController');
 
 // Define the route to get all policies
-router.get('/policies', policyController.getAllPolicies);
+router.get('/policies', authenticateToken, policyController.getAllPolicies);
 
 // Define the route to get policy by ID
-router.get('/policies/:id', policyController.getPolicyById);
+router.get('/policies/:id', authenticateToken, policyController.getPolicyById);
 
 // Define the route to create a new policy
-router.post('/policies', policyController.createPolicy);
+router.post('/policies', authenticateToken, policyController.createPolicy);
 
 module.exports = router;
