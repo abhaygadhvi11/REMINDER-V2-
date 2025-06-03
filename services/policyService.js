4d6484ebb2d8e0d063e2cafa4293f4024506f768
Abhay Gadhvi <gadhviabhay11@gmail.com>
2025-06-03 12:11:47 +0530

    Add files via upload

--- services/policyService.js
+++ services/policyService.js
@@ -1,21 +1,28 @@
 const policyRepository = require('../repositories/policyRepository');
 
 // Function to get all policies using the repository
+// const getAllPolicies = (callback) => {
+//   policyRepository.getAllPolicies((err, policies) => {
+//     if (err) {
+//       //console.error('Error in service while fetching policies:', err);
+//       return callback(err);
+//     } 
+//     callback(null, policies);
+//   });
+// };
 const getAllPolicies = (callback) => {
   policyRepository.getAllPolicies((err, policies) => {
     if (err) {
-      console.error('Error in service while fetching policies:', err);
       return callback(err);
     } 
-    callback(null, policies);
+    callback(null, policies); // should be an array of objects
   });
 };
-
 // Function to get policy by ID using the repository    
 const getPolicyById = (policyId, callback) => {
   policyRepository.getPolicyById(policyId, (err, policy) => {
     if (err) {
-      console.error('Error in service while fetching policy by ID:', err);
+      //console.error('Error in service while fetching policy by ID:', err);
       return callback(err);
     }
     callback(null, policy);
@@ -26,7 +33,7 @@ const getPolicyById = (policyId, callback) => {
 const createPolicy = (policyData, callback) => {
   policyRepository.createPolicy(policyData, (err, policyId) => {
     if (err) {
-      console.error('Error in service while creating policy:', err);
+      //console.error('Error in service while creating policy:', err);
       return callback(err);
     }
     callback(null, policyId);
